3d896476d3160a2218c8e9aa0072686f9f072438
yileiren <yileiren@126.com>
2012-04-27 14:48:47 +0800

    添加SQLite数据库类库。

---
+++ YAdoNet/YSqliteDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace YLR.YAdoNet
+{
+    /// <summary>
+    /// Sqlite数据库操作封装。
+    /// </summary>
+    public class YSqliteDataBase
+    {
+        SQLiteConnection s = new SQLiteConnection();
+    }
+}
