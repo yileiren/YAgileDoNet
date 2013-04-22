7594c1aa0f2e09df0c80767daa870f4444c9e575
yileiren <yileiren@126.com>
2013-04-22 23:45:04 +0800

    完成DES加密算法。

--- Test/YCryptoTest.cs
+++ Test/YCryptoTest.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using YLR.YCrypto;
+using System.Security.Cryptography;
 
 namespace Test
 {
@@ -22,6 +23,13 @@ namespace Test
             string bbb = Convert.ToBase64String(AESEncrypt.encrypt("AES加密算法测试数据", "dongbinhuiasxiny"));
             MessageBox.Show(bbb);
             MessageBox.Show(Encoding.UTF8.GetString(AESEncrypt.decrypt(Convert.FromBase64String(bbb), "dongbinhuiasxiny")));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string bbb = Convert.ToBase64String(DESEncrypt.encrypt("DES加密算法测试数据", "12345678"));
+            MessageBox.Show(bbb);
+            MessageBox.Show(Encoding.UTF8.GetString(DESEncrypt.decrypt(Convert.FromBase64String(bbb), "12345678")));
         }
     }
 }
---
+++ YCrypto/DESEncrypt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace YLR.YCrypto
+{
+    /// <summary>
+    /// DES加密算法处理类。
+    /// </summary>
+    public class DESEncrypt
+    {
+        /// <summary>  
+        /// DES加密算法  
+        /// </summary>  
+        /// <param name="plainText">明文字符串</param>  
+        /// <param name="strKey">密钥，支持8位密钥</param>  
+        /// <returns>返回加密后的密文字节数组，可以使用Convert.ToBase64String方法将字节数组转换成字符串。</returns>
+        public static byte[] encrypt(string plainText, string strKey)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(strKey);
+            byte[] keyIV = keyBytes;
+            byte[] inputByteArray = Encoding.UTF8.GetBytes(plainText);
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, provider.CreateEncryptor(keyBytes, keyIV), CryptoStreamMode.Write);
+            cStream.Write(inputByteArray, 0, inputByteArray.Length);
+            cStream.FlushFinalBlock();
+            return mStream.ToArray();
+        }
+
+        /// <summary>  
+        /// DES解密  
+        /// </summary>  
+        /// <param name="cipherText">密文字节数组，可以使用Convert.FromBase64String方法从字符串获取字节数组。</param>  
+        /// <param name="strKey">密钥，支持8位</param>  
+        /// <returns>返回解密后的字符串</returns> 
+        public static byte[] decrypt(byte[] cipherText, string strKey)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(strKey);
+            byte[] keyIV = keyBytes;
+            byte[] inputByteArray = cipherText;
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, provider.CreateDecryptor(keyBytes, keyIV), CryptoStreamMode.Write);
+            cStream.Write(inputByteArray, 0, inputByteArray.Length);
+            cStream.FlushFinalBlock();
+            return mStream.ToArray();
+        }
+    }
+}
